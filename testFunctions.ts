17ca18da6b9cc861722b96f8965ce970cba4764c
Utsav Khatiwada <utsavk3210@gmail.com>
2025-01-28 17:56:36 -0800

    Removed the files

--- testFunctions.ts
+++
@@ -1,6 +0,0 @@
-function removeDuplicates(arr: number[]): number[] {
-  console.log(Array.from(new Set(arr)));
-  return Array.from(new Set(arr));
-}
-
-removeDuplicates([1, 2, 3, 3, 4, 5, 5, 6]); // [1, 2, 3, 4, 5, 6]
