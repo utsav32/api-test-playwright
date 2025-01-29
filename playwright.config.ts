a98f39783b00aa3f04eb44483c24e230866c5b93
Utsav Khatiwada <utsavk3210@gmail.com>
2025-01-29 14:44:01 -0800

    Update config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,46 +1,15 @@
-import { defineConfig, devices } from "@playwright/test";
+import { defineConfig } from "@playwright/test";
 import dotenv from "dotenv";
-
 export default defineConfig({
-  // Look for test files in the "tests" directory, relative to this configuration file.
-  testDir: "tests",
-
-  // Run all tests in parallel.
-  fullyParallel: true,
-
-  // Fail the build on CI if you accidentally left test.only in the source code.
-  forbidOnly: !!process.env.CI,
-
-  // Retry on CI only.
-  retries: process.env.CI ? 2 : 0,
-
-  // Opt out of parallel tests on CI.
-  workers: process.env.CI ? 1 : undefined,
-
-  // Reporter to use
-  reporter: "html",
-
   use: {
-    // Base URL to use in actions like `await page.goto('/')`.
-    baseURL: "http://127.0.0.1:3000",
-
-    // Collect trace when retrying the failed test.
-    trace: "on-first-retry",
-
-    //headless mode
-    headless: true,
-  },
-  // Configure projects for major browsers.
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+    // All requests we send go to this API endpoint.
+    baseURL: "https://comsolotex-admin.sologenic.org/api",
+    extraHTTPHeaders: {
+      // We set this header per GitHub guidelines.
+      Accept: "application/json",
+      // Add authorization token to all requests.
+      // Assuming personal access token available in the environment.
+      Authorization: `${process.env.API_TOKEN}`,
     },
-  ],
-  // Run your local dev server before starting the tests.
-  webServer: {
-    command: "npm start", // This will now work
-    port: 3000, // Ensure this matches your app's port
-    reuseExistingServer: !process.env.CI,
   },
 });
